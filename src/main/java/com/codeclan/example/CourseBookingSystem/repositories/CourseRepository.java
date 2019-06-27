f19cd59fa6b53d2d828933f7ef2637bfe103a5cf
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-06-27 11:39:33 +0100

    can get courses by star rating

--- src/main/java/com/codeclan/example/CourseBookingSystem/repositories/CourseRepository.java
+++ src/main/java/com/codeclan/example/CourseBookingSystem/repositories/CourseRepository.java
@@ -3,5 +3,10 @@ package com.codeclan.example.CourseBookingSystem.repositories;
 import com.codeclan.example.CourseBookingSystem.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findCoursesByStarRating(int rating);
+
 }
--- src/test/java/com/codeclan/example/CourseBookingSystem/CourseBookingSystemApplicationTests.java
+++ src/test/java/com/codeclan/example/CourseBookingSystem/CourseBookingSystemApplicationTests.java
@@ -1,9 +1,18 @@
 package com.codeclan.example.CourseBookingSystem;
 
+import com.codeclan.example.CourseBookingSystem.models.Course;
+import com.codeclan.example.CourseBookingSystem.repositories.BookingRepository;
+import com.codeclan.example.CourseBookingSystem.repositories.CourseRepository;
+import com.codeclan.example.CourseBookingSystem.repositories.CustomerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +20,23 @@ public class CourseBookingSystemApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Autowired
+	BookingRepository bookingRepository;
+
+
+
+	@Test
+	public void canGetCoursesByrating() {
+		List<Course> found = courseRepository.findCoursesByStarRating(4);
+		assertEquals(2, found.size());
 	}
 
 }
