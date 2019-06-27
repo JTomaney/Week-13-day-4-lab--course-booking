67101b7a2e8474355221d0fe75ac0e3a63e7db70
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-06-27 11:29:45 +0100

    more seeds

--- src/main/java/com/codeclan/example/CourseBookingSystem/components/DataLoader.java
+++ src/main/java/com/codeclan/example/CourseBookingSystem/components/DataLoader.java
@@ -31,12 +31,32 @@ public class DataLoader implements ApplicationRunner {
         Customer customer1 = new Customer("Bill", "Edinburgh", 57);
         customerRepository.save(customer1);
 
+        Customer customer2 = new Customer("Bob", "Edinburgh", 44);
+        customerRepository.save(customer2);
+
+        Customer customer3 = new Customer("Jeb", "Glasgow", 84);
+        customerRepository.save(customer3);
+
         Course course1 = new Course("Billing 101", "Edinburgh", 4);
         courseRepository.save(course1);
 
-        Booking booking1 = new Booking("30-06-19", customer1, course1);
+        Course course2 = new Course("Billing 102", "Edinburgh", 3);
+        courseRepository.save(course2);
+
+        Course course3 = new Course("Basic Rocket Science", "Inverness", 4);
+        courseRepository.save(course3);
+
+        Booking booking1 = new Booking("30-06-18", customer1, course1);
         bookingRepository.save(booking1);
 
+        Booking booking2 = new Booking("10-07-19", customer1, course2);
+        bookingRepository.save(booking2);
+
+        Booking booking3 = new Booking("27-08-19", customer3, course3);
+        bookingRepository.save(booking3);
+
+        Booking booking4 = new Booking("27-08-19", customer2, course3);
+        bookingRepository.save(booking4);
 
     }
 }
